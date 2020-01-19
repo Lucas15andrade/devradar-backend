9130d53fc90ba94a59590d442691db43714b3718
lucas15andrade <lap.andrade15@gmail.com>
2020-01-18 22:14:22 -0300

    Versão final

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const http = require('http');
 const cors = require('cors');
+const {setupWebSocket} = require('./websocket');
 
 const app = express();
+const server = http.Server(app);
+
+setupWebSocket(server);
 
 mongoose.connect('mongodb+srv://andrade:00798@cluster0-wnxgu.mongodb.net/week10?retryWrites=true&w=majority', {
     useNewUrlParser: true,
@@ -16,4 +21,4 @@ app.use(express.json());
 app.use(routes);
 
 
-app.listen(3333);
+server.listen(3333);
